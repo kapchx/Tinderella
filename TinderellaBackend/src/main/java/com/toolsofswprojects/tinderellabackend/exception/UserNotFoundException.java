ae5e70036d92dc4ec21167de05e3b2b2a4d799ed
Ardi Shehu <g3vl2z@inf.elte.hu>
2021-10-07 22:22:53 +0200

    Revert "UserService and UserNotFoundException added"
    This reverts commit d8fe52fd2f5e6a241d52c0f717f200bb42f8e91d.

--- TinderellaBackend/src/main/java/com/toolsofswprojects/tinderellabackend/exception/UserNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.toolsofswprojects.tinderellabackend.exception;
-
-public class UserNotFoundException extends RuntimeException{
-    public UserNotFoundException(String message) {
-        super(message);
-    }
-}
